8eae8d146a29f8587b4a0b20e0deb8c094c369e6
Dheepak Krishnamurthy <me@kdheepak.com>
2021-12-28 16:12:49 -0700

    Update files for parcel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  mode: "jit",
+  content: ['./index.html', './src/**/*.{html,vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
+  variants: {},
   plugins: [],
-}
+};
