d11cd949ca51330d11bda45bb635eacad943a3ff
Dheepak Krishnamurthy <me@kdheepak.com>
2021-12-29 13:53:25 -0700

    chore: Update files ♻️

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,9 @@
 <script setup>
+import { onMounted, ref, watchEffect, computed } from 'vue'
 import * as d3 from 'd3'
-import { ref, reactive } from 'vue'
+import Graph from './Graph.vue'
 
 function create_worker() {
-  console.log('worker')
   return new Worker(new URL('../workers/matpower.js', import.meta.url), {
     type: 'module',
   })
@@ -11,16 +11,36 @@ function create_worker() {
 
 const worker = create_worker()
 
-const data = {
-  case_obj: {},
-  loading: false,
-  loaded: false,
-}
-const state = reactive(data)
+const case_obj = ref({})
+const loading = ref(false)
+const loaded = ref(false)
+const case_graph = computed(() => {
+  if (case_obj.value.branch === undefined) {
+    return {
+      nodes: [],
+      links: [],
+    }
+  } else {
+    const node_names = case_obj.value.bus_name
+    const nodes = case_obj.value.bus.map((element, i) => {
+      return {
+        id: element.idx,
+        name: node_names[i] ? node_names[i] : element.idx,
+      }
+    })
+    const links = case_obj.value.branch.map((element) => {
+      return { source: element.f_bus, target: element.t_bus }
+    })
+    return {
+      nodes: nodes,
+      links: links,
+    }
+  }
+})
 
 function uploadFile(e) {
-  state.loaded = false
-  state.loading = true
+  loaded.value = false
+  loading.value = true
   const file = e.target.files[0]
   if (file) {
     var reader = new FileReader()
@@ -32,15 +52,16 @@ function uploadFile(e) {
     }
 
     worker.addEventListener('message', (event) => {
-      state.case_obj = event.data.data
-      state.loaded = true
-      state.loading = false
+      case_obj.value = event.data.data
+      console.log(case_obj.value)
+      loaded.value = true
+      loading.value = false
     })
 
     reader.onerror = function (_) {
-      state.case_obj = null
-      state.loading = false
-      state.loaded = false
+      case_obj.value = null
+      loading.value = false
+      loaded.value = false
     }.bind(this)
   }
 }
@@ -50,15 +71,9 @@ function uploadFile(e) {
   <div>
     <input @change="uploadFile" type="file" ref="file" />
   </div>
-  <div v-if="state.loading">Loading...</div>
-  <div v-if="state.loaded">
-    Number of buses: {{ state.case_obj.bus.length }}
-  </div>
-  <div v-if="state.loaded">
-    Number of generators: {{ state.case_obj.gen.length }}
-  </div>
-  <div v-if="state.loaded">
-    Number of branches: {{ state.case_obj.branch.length }}
-  </div>
-  <div id="plot" />
+  <div v-if="loading">Loading...</div>
+  <div v-if="loaded">Number of buses: {{ case_obj.bus.length }}</div>
+  <div v-if="loaded">Number of generators: {{ case_obj.gen.length }}</div>
+  <div v-if="loaded">Number of branches: {{ case_obj.branch.length }}</div>
+  <Graph :data="case_graph" />
 </template>
