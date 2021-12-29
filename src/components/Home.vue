f09e411ce512e54fed2733ba307d5cc513b44a3d
Dheepak Krishnamurthy <me@kdheepak.com>
2021-12-29 10:57:10 -0700

    chore: Update files ♻️

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,9 @@
-<script>
+<script setup>
 import * as d3 from 'd3'
+import { ref } from 'vue'
 
 function create_worker() {
+  console.log('worker')
   return new Worker(new URL('../workers/matpower.js', import.meta.url), {
     type: 'module',
   })
@@ -9,56 +11,37 @@ function create_worker() {
 
 const worker = create_worker()
 
-export default {
-  name: 'Home',
-  data() {
-    return {
-      case_obj: {},
-      loading: false,
-      loaded: false,
+const state = ref({
+  case_obj: {},
+  loading: false,
+  loaded: false,
+})
+
+function uploadFile(e) {
+  state.value.loaded = false
+  state.value.loading = true
+  const file = e.target.files[0]
+  if (file) {
+    var reader = new FileReader()
+    reader.readAsText(file, 'UTF-8')
+    reader.onload = function (evt) {
+      worker.postMessage({
+        data: evt.target.result,
+      })
     }
-  },
-  mounted() {
-    this.generatePlot()
-  },
-  methods: {
-    generatePlot() {
-      const w = 500
-      const h = 500
-      const svg = d3
-        .select('#plot')
-        .append('svg')
-        .attr('width', w)
-        .attr('height', h)
-    },
-    uploadFile(e) {
-      this.loaded = false
-      this.loading = true
-      const file = e.target.files[0]
-      const that = this
-      if (file) {
-        var reader = new FileReader()
-        reader.readAsText(file, 'UTF-8')
-        reader.onload = function (evt) {
-          worker.postMessage({
-            data: evt.target.result,
-          })
-        }
 
-        worker.addEventListener('message', (event) => {
-          that.case_obj = event.data.data
-          that.loaded = true
-          that.loading = false
-        })
+    worker.addEventListener('message', (event) => {
+      state.value.case_obj = event.data.data
+      state.value.loaded = true
+      state.value.loading = false
+    })
 
-        reader.onerror = function (_) {
-          this.case_obj = null
-          this.loading = false
-          this.loaded = false
-        }.bind(this)
-      }
-    },
-  },
+    reader.onerror = function (_) {
+      state.value.case_obj = null
+      state.value.loading = false
+      state.value.loaded = false
+    }.bind(this)
+  }
 }
 </script>
 
@@ -66,9 +49,15 @@ export default {
   <div>
     <input @change="uploadFile" type="file" ref="file" />
   </div>
-  <div v-if="loading">Loading...</div>
-  <div v-if="loaded">Number of buses: {{ case_obj.bus.length }}</div>
-  <div v-if="loaded">Number of generators: {{ case_obj.gen.length }}</div>
-  <div v-if="loaded">Number of branches: {{ case_obj.branch.length }}</div>
+  <div v-if="state.loading">Loading...</div>
+  <div v-if="state.loaded">
+    Number of buses: {{ state.case_obj.bus.length }}
+  </div>
+  <div v-if="state.loaded">
+    Number of generators: {{ state.case_obj.gen.length }}
+  </div>
+  <div v-if="state.loaded">
+    Number of branches: {{ state.case_obj.branch.length }}
+  </div>
   <div id="plot" />
 </template>
